ee31e29659e3fc7993f9c428f3da3516ecb159b1
june <june3360@gmail.com>
2019-11-29 17:03:56 +0900

    greeting.js done

--- greeting.js
+++ greeting.js
@@ -4,12 +4,20 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
-function askForName() {
-    form.classList.add(SHOWING_CN);
+function saveName(text) {
+    localStorage.setItem(USER_LS, text)
+}
+    
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
 }
 
-function handleSubmit(event) {
-   event.preventDefault();
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
 }
 
 function paintGreeting(text) {
